f75b36105e4d77e6ab1cd41f57ee77b31bb61f7f
Amaresh Jana <amaresh.jana26@gmail.com>
2017-05-28 19:26:15 +0530

    first program not written any code just declared the method

---
+++ Problem 1/src/DuplicateArray.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author amareshjana
+ * @question remove the duplicates from the array 
+ *
+ */
+public class DuplicateArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 *@param given array with duplicate numbers and need to remove the duplicates from the array
+	 *@return an array with out duplicates 
+	 * */
+	public static int[] removeDuplicates(int[] array){
+		return null;
+	}
+
+}
