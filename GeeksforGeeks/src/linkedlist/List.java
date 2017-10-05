b0f5a2b4a817f49d35fbd9310d2704d2ab29522e
Amaresh Jana <amaresh.jana26@gmail.com>
2017-10-05 11:28:57 +0530

    Implemented the linked list
    Added the list interface, ListNode and LinkedList

---
+++ GeeksforGeeks/src/linkedlist/List.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package linkedlist;
+
+/**
+ * @author amareshjana
+ *
+ */
+public interface List {
+	/***
+	 * this is to add the element to the list
+	 * */
+	 void addElement(int element);
+	 void deletElement(int element);
+	 void displayElements();
+	 void getElement(int position);
+}
---
+++ GeeksforGeeks/src/linkedlist/ListNode.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package linkedlist;
+
+/**
+ * @author amareshjana
+ *
+ */
+public class ListNode {
+
+	private ListNode nextNode;
+	private int data;
+
+
+	/**
+	 * @param nextNode
+	 * @param data
+	 */
+	public ListNode(ListNode nextNode, int data) {
+		super();
+		this.nextNode = nextNode;
+		this.data = data;
+	}
+
+
+	/**
+	 * constructor with no parameters
+	 */
+	public ListNode() {
+		
+	}
+
+	/**
+	 * @return the nextNode
+	 */
+	public ListNode getNextNode() {
+		return nextNode;
+	}
+	/**
+	 * @param nextNode the nextNode to set
+	 */
+	public void setNextNode(ListNode nextNode) {
+		this.nextNode = nextNode;
+	}
+	/**
+	 * @return the data
+	 */
+	public int getData() {
+		return data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(int data) {
+		this.data = data;
+	}
+}
